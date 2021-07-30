bebc9c1a700ae7f0392a36968ec4f210ad647f72
Maziarmajd <83129200+Maziarmajd@users.noreply.github.com>
2021-07-30 14:35:14 +0100

    Exercises B

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,14 @@
-function shoppingList(arrayOfPeople) {
+function shoppingList(arrayOfShoppingList) {
   // Write your code here...
+    const content = document.querySelector("#content"); //<------- target the div element to assign ul
+    const createUl = document.createElement("ul"); //<----------- create <ul></ul>
+    arrayOfShoppingList.forEach((product) => {
+    const createList = document.createElement("li"); //<---------- create list
+    createList.innerText = product;
+    createUl.appendChild(createList); //<------------ child element in ul
+  });
+
+  content.append(createUl);
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
