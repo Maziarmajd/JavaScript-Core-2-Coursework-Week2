4616a6e671c66ffb31c3ea6cbc17e517cf2a8b6d
Maziarmajd <83129200+Maziarmajd@users.noreply.github.com>
2021-07-30 14:31:26 +0100

    exerscise A

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,11 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+   arrayOfPeople.forEach((person) => {
+    const createH1 = document.createElement("h1");
+    const createH2 = document.createElement("h2");
+    createH1.innerText = person.name;
+    createH2.innerText = person.job;
+    content.append(createH1, createH2);
 }
 
 let people = [
